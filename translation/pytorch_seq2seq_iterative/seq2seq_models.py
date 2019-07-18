cb2929c199795cd68ad257f90738a0468111bd18
Shuvendu Bikash <bikash.shuvendu@gmail.com>
2019-07-18 10:33:22 +0600

    annotated transformer
    Signed-off-by: Shuvendu Bikash <bikash.shuvendu@gmail.com>

---
+++ translation/pytorch_seq2seq_iterative/seq2seq_models.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+MAX_LENGTH = 100
+
+SOS_token = chr(0)
+EOS_token = 1
+
+
+def cuda_variable(tensor):
+    if torch.cuda.is_available():
+        return Variable(tensor.cuda())
+    else:
+        return Variable(tensor)
+
+
+# string to char tensor
+def str2tensor(msg, eos=False):
+    tensor = [ord(c) for c in msg]
+    if eos:
+        tensor.append(EOS_token)
+
+    return cuda_variable(torch.LongTensor(tensor))
+
+
+class EncoderRNN(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers=1):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.n_layers = num_layers
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, num_layers)
+
+    def forward(self, word_inputs, hidden):
+        seq_len = len(word_inputs)
+        # formate shape
+        embedded = self.embedding(word_inputs).view(seq_len, 1, -1)  # gru input shape = (seq_len, batch_dim, feature)
+        output, hidden = self.gru(embedded, hidden)
+        return output, hidden
+
+    def init_hidden(self):
+        # (num_layers * num_directions, batch, hidden_size)
+        return cuda_variable(torch.zeros(self.n_layers, 1, self.hidden_size))
+
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, n_layers=1):
+        super(DecoderRNN, self).__init__()
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output, hidden = self.gru(output, hidden)
+        output = self.out(output[0])
+
+        return output, hidden
+
+    def init_hidden(self):
+        return cuda_variable(torch.zeros(self.n_layers, 1, hidden_size))
---
+++ translation/pytorch_seq2seq_iterative/text_loader.py
@@ -0,0 +1,41 @@
+import gzip
+from torch.utils.data import Dataset, DataLoader
+import os
+
+# define root datadir
+if os.path.exists('E:/Datasets'):
+    root = 'E:/Datasets'
+elif os.path.exists('C:/Users/user/Documents/datasets'):
+    root = 'C:/Users/user/Documents/datasets'
+else:
+    root = '.'
+    
+    
+class TextDataset(Dataset):
+    
+    def __init__(self, filename='shakespeare.txt.gz'):
+        self.len = 0
+        self.filename = root+'/NLP_data/'+filename
+        
+        with gzip.open(self.filename, 'rt') as f:
+            self.targetLines = [x.strip() for x in f if x.strip()]
+            self.srcLines = [x.lower().replace(' ', '')
+                             for x in self.targetLines]
+            self.len = len(self.srcLines)
+            
+    def __getitem__(self, index):
+        return self.srcLines[index], self.targetLines[index]
+
+    def __len__(self):
+        return self.len
+
+
+# Test the loader
+if __name__ == "__main__":
+    dataset = TextDataset()
+    train_loader = DataLoader(dataset=dataset,
+                              batch_size=3,
+                              shuffle=True)
+
+    for i, (src, target) in enumerate(train_loader):
+        print(i, "data", src)    
